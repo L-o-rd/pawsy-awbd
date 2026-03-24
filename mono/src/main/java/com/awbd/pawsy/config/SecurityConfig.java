9970f73d8236f70b1eb122f668f8dde1b21686b5
L-o-rd <musatfabiandg@gmail.com>
2026-03-24 14:30:51 +0200

    feat(security): Guests can now visit the usual pages.

---
+++ mono/src/main/java/com/awbd/pawsy/config/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.awbd.pawsy.config;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth
+                .requestMatchers("/", "/login", "/register", "/css/**", "/images/**").permitAll()
+                .anyRequest().authenticated()).formLogin(login -> login
+                        .loginPage("/login")
+                        .defaultSuccessUrl("/", true)
+                        .permitAll()).logout(logout -> logout
+                        .logoutSuccessUrl("/"));
+
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
