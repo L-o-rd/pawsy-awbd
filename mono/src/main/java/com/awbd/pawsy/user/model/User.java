ec632003d94023008ae39b4e54ef2dd26e82d0e9
L-o-rd <musatfabiandg@gmail.com>
2026-03-24 14:29:17 +0200

    feat(users): Added a starter User entity.

---
+++ mono/src/main/java/com/awbd/pawsy/user/model/User.java
@@ -0,0 +1,43 @@
+package com.awbd.pawsy.user.model;
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+    private String phone;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
