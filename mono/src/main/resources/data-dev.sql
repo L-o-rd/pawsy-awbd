050090c41a9b8d7218133df61226e342055f5fc3
L-o-rd <musatfabiandg@gmail.com>
2026-04-09 12:03:34 +0300

    build: Added Pet & Shelter data.

--- mono/src/main/resources/data-dev.sql
+++ mono/src/main/resources/data-dev.sql
@@ -2,5 +2,8 @@ insert into roles (id, name) values (1, 'Adopter');
 insert into roles (id, name) values (2, 'Manager');
 insert into roles (id, name) values (3, 'Administrator');
 
-insert into pets (id, name, species, age, description, photo, status, sex)
-values (1, 'Milo', 'Cat', 3, 'Playful', null, 'Available', 'Male');
+insert into shelters (id, email, location, name, phone)
+values (1, 'happy-paws@mail.com', 'Bucharest', 'Happy Paws', '0123456789');
+
+insert into pets (id, name, species, age, description, photo, status, sex, shelter_id)
+values (1, 'Milo', 'Cat', 3, 'Playful', null, 'Available', 'Male', 1);
